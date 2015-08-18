092c07bd549ad0fa7d7a0efa80526d570fad63ca
Joel Dewey <jdewey@nexient.com>
2015-08-18 16:46:31 -0400

    Created a Person POJO with an appropriate Gender.

---
+++ src/main/java/com/boredblog/enums/Gender.java
@@ -0,0 +1,11 @@
+package com.boredblog.enums;
+
+/**
+ * @author Joel Dewey
+ * @date 8/18/2015
+ * Group: Joel
+ * Binary genders.
+ */
+public enum Gender {
+    MALE, FEMALE
+}
---
+++ src/main/java/com/boredblog/model/Person.java
@@ -0,0 +1,22 @@
+package com.boredblog.model;
+
+import com.boredblog.enums.Gender;
+
+/**
+ * @author Joel Dewey
+ * @date 8/18/2015
+ * Group: Joel
+ * Describes a person in terms of age and gender.
+ */
+public class Person {
+    private int age;
+    private Gender gender;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
