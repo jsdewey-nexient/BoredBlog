1590dff54d16eb365858e09312f72c16698d5d4d
Joel Dewey <jdewey@nexient.com>
2015-08-20 13:41:12 -0400

    User is now Author.

--- src/main/java/com/boredblog/entity/Author.java
+++ src/main/java/com/boredblog/entity/Author.java
@@ -12,7 +12,7 @@ import java.util.List;
  */
 @Entity
 @Table(name = "users")
-public class User extends BaseEntity {
+public class Author extends BaseEntity {
     @Column(name = "first_name", nullable = false)
     private String firstName;
     @Column(name = "last_name", nullable = false)
