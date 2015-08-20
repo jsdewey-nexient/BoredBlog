5d04510a2988e3f6b6f9fb83d16e266441648ca3
Joel Dewey <jdewey@nexient.com>
2015-08-20 11:52:30 -0400

    Created User entity.

--- src/main/java/com/boredblog/entity/User.java
+++ src/main/java/com/boredblog/entity/User.java
@@ -1,30 +1,60 @@
 package com.boredblog.entity;
 
 import com.boredblog.enums.Gender;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 /**
  * @author Joel Dewey
  * @date 8/18/2015
  * Group: Joel
- * Describes a person in terms of age and personGender.
+ * Describes a user of the blog. He/she may add posts and edit new ones.
  */
-public class User {
-    private int age;
-    private Gender personGender;
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(name = "screen_name", nullable = false)
+    private String screenname;
+    @JsonIgnore
+    @Column(name = "password", nullable = false)
+    private String password;
 
-    public int getAge() {
-        return age;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public Gender getPersonGender() {
-        return personGender;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setPersonGender(Gender personGender) {
-        this.personGender = personGender;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getScreenname() {
+        return screenname;
+    }
+
+    public void setScreenname(String screenname) {
+        this.screenname = screenname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
