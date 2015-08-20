caedc35ceb0fd7a71c0531fe70eda7459972bf4d
Joel Dewey <jdewey@nexient.com>
2015-08-20 11:34:34 -0400

    Added BaseEntity for all other entities to inherit from.

---
+++ src/main/java/com/boredblog/entity/BaseEntity.java
@@ -0,0 +1,51 @@
+package com.boredblog.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+/**
+ * @author Joel Dewey
+ * @date 8/20/2015
+ * Group: Joel
+ * A base entity containing shared properties for all other entities to inherit
+ * from.
+ */
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", unique = true, nullable = false)
+    private Integer id;
+    @JsonIgnore
+    @Column(name = "created_at", nullable = false)
+    private Timestamp createdAt;
+    @JsonIgnore
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updatedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
