402238875e7b67a3ce066d1d5e52195d91c4181a
Joel Dewey <jdewey@nexient.com>
2015-08-20 13:50:56 -0400

    Removed some old code.

--- src/main/java/com/boredblog/controller/UserController.java
+++ src/main/java/com/boredblog/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.boredblog.controller;
 
-import com.boredblog.entity.User;
+import com.boredblog.entity.Author;
 import com.boredblog.enums.Gender;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,17 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Joel Dewey
  * @date 8/18/2015
  * Group: Joel
- * Responsible for delivering User objects.
+ * Responsible for delivering Author objects.
  */
 @RestController
 public class UserController {
-
-    @RequestMapping(name = "person", method = RequestMethod.GET)
-    public User read() {
-        User user = new User();
-        user.setAge(23);
-        user.setPersonGender(Gender.MALE);
-
-        return user;
-    }
 }
