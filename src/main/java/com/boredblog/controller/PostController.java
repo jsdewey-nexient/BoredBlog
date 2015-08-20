07b2e4ff4664e786878b21f6cdbc83c87380fd19
Joel Dewey <jdewey@nexient.com>
2015-08-20 16:25:37 -0400

    Created controller and test file.

---
+++ src/main/java/com/boredblog/controller/PostController.java
@@ -0,0 +1,10 @@
+package com.boredblog.controller;
+
+/**
+ * @author Joel Dewey
+ * @date 8/20/2015
+ * Group: Joel
+ * Responsible for the delivery of posts.
+ */
+public class PostController {
+}
---
+++ src/test/java/com/boredblog/controller/PostControllerTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.controller;
+
+/**
+ * @author Joel Dewey
+ * @date 8/20/2015
+ * Group: Joel
+ * Tests the functions of the PostController
+ */
+public class PostControllerTest {
+}
