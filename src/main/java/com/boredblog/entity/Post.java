e4b2985e7c9975e5bc50c544c3cde301f5c9ad5a
Joel Dewey <jdewey@nexient.com>
2015-08-20 11:58:41 -0400

    Added methods and annotations to Post.

--- src/main/java/com/boredblog/entity/Post.java
+++ src/main/java/com/boredblog/entity/Post.java
@@ -1,10 +1,51 @@
 package com.boredblog.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+
 /**
  * @author Joel Dewey
  * @date 8/20/2015
  * Group: Joel
  * Describes a blog post made by the user.
  */
-public class Post {
+@Entity
+@Table(name = "posts")
+public class Post extends BaseEntity {
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    @JsonProperty("created_at")
+    public Timestamp getCreatedAt() {
+        return super.getCreatedAt();
+    }
+
+    @Override
+    @JsonProperty("updated_at")
+    public Timestamp getUpdatedAt() {
+        return super.getUpdatedAt();
+    }
 }
