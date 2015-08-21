37c7d83e165d521d291f678b3639a5c561f084a4
Joel Dewey <jdewey@nexient.com>
2015-08-21 12:53:59 -0400

    Added constants to test.

--- src/test/java/com/boredblog/entity/AuthorTest.java
+++ src/test/java/com/boredblog/entity/AuthorTest.java
@@ -7,4 +7,8 @@ package com.boredblog.entity;
  * Verify that the Author entity works as expected.
  */
 public class AuthorTest {
+    private final String FIRST_NAME = "Johnny";
+    private final String LAST_NAME = "Nexient";
+    private final String SCREEN_NAME = "jNexient2";
+
 }
