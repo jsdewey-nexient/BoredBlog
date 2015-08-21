86e209a7c74f51f7deb0f1b228f4b71624763d4e
Joel Dewey <jdewey@nexient.com>
2015-08-21 11:22:59 -0400

    Wrote unit tests for CommentController.

--- src/test/java/com/boredblog/controller/CommentControllerTest.java
+++ src/test/java/com/boredblog/controller/CommentControllerTest.java
@@ -2,6 +2,7 @@ package com.boredblog.controller;
 
 import com.boredblog.entity.Comment;
 import com.boredblog.manager.CommentManager;
+import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -10,6 +11,8 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.List;
+
+import static org.junit.Assert.*;
 
 /**
  * @author Joel Dewey
@@ -39,22 +42,49 @@ public class CommentControllerTest {
 
     @Test
     public void testCreatingComment() {
+        Comment result = this.commentController.createComment(
+                POST_ID,
+                this.comment
+        );
 
+        assertEquals("testCreatingComment did not receive the proper " +
+                "Comment object back.", this.comment, result);
+        Mockito.verify(this.commentManager).create(POST_ID, this.comment);
     }
 
     @Test
     public void testUpdatingComment() {
+        Comment result = this.commentController.updateComment(
+                POST_ID,
+                COMMENT_ID,
+                this.comment
+        );
 
+        assertEquals("testUpdatingComment did not receive the updated " +
+                "Comment object back.", this.updatedComment, result);
+        Mockito.verify(this.commentManager).update(
+                POST_ID,
+                COMMENT_ID,
+                this.comment
+        );
     }
 
     @Test
     public void testRetrievingAllCommentsForPost() {
+        List<Comment> result = this.commentController.getComments(POST_ID);
 
+        assertEquals("testRetrievingAllCommentsForPost did not receive the " +
+                "List of Comments back.", this.comments, result);
+        Mockito.verify(this.commentManager).retrieveAll(POST_ID);
     }
 
     @Test
     public void testRetrievingSingleCommentForPost() {
+        Comment result = this.commentController.getComment(POST_ID, COMMENT_ID);
 
+        assertEquals("testRetrievingSingleCommentForPost did not receive the " +
+                "correct Comment object back.", this.comment, result);
+        Mockito.verify(this.commentManager).retrieve(POST_ID, COMMENT_ID);
     }
 
     private void mockCommentManager() {
@@ -62,7 +92,11 @@ public class CommentControllerTest {
                 .thenReturn(this.comment);
         Mockito.when(this.commentManager.create(POST_ID, this.comment))
                 .thenReturn(this.comment);
-        Mockito.when(this.commentManager.update(POST_ID, this.comment))
+        Mockito.when(this.commentManager.update(
+                POST_ID,
+                COMMENT_ID,
+                this.comment
+        ))
                 .thenReturn(this.updatedComment);
         Mockito.when(this.commentManager.retrieveAll(POST_ID))
                 .thenReturn(this.comments);
