c59dc1054eb493ab0f46bfe77bf531c4d2faddad
Joel Dewey <jdewey@nexient.com>
2015-08-21 14:13:21 -0400

    Created test suite for Comment entity.

---
+++ src/test/java/com/boredblog/entity/CommentTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.entity;
+
+/**
+ * @author Joel Dewey
+ * @date 8/21/2015
+ * Group: Joel
+ * Verify that a Comment entity works as it should.
+ */
+public class CommentTest {
+}
