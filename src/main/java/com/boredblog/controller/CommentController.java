3a0bea199d360c7bfe695e9d66943867fa3f472d
Joel Dewey <jdewey@nexient.com>
2015-08-21 10:14:10 -0400

    Created CommentController.

---
+++ src/main/java/com/boredblog/controller/CommentController.java
@@ -0,0 +1,10 @@
+package com.boredblog.controller;
+
+/**
+ * @author Joel Dewey
+ * @date 8/21/2015
+ * Group: Joel
+ * Loads comments for a particular post.
+ */
+public class CommentController {
+}
