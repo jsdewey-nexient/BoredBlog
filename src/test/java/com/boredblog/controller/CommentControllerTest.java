2395ffa214da0219bd4a97c10fe65229c93f301a
Joel Dewey <jdewey@nexient.com>
2015-08-21 10:15:15 -0400

    Added test file for CommentController.

---
+++ src/test/java/com/boredblog/controller/CommentControllerTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.controller;
+
+/**
+ * @author Joel Dewey
+ * @date 8/21/2015
+ * Group: Joel
+ * Verify that comments are returned correctly.
+ */
+public class CommentControllerTest {
+}
