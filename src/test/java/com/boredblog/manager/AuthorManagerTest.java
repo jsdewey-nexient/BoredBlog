308ec6baa107fc5c74484ef1dcaf06e9a38a3ac8
Joel Dewey <jdewey@nexient.com>
2015-08-21 15:24:24 -0400

    Created AuthorManager test suite.

---
+++ src/test/java/com/boredblog/manager/AuthorManagerTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.manager;
+
+/**
+ * @author Joel Dewey
+ * @date 8/21/2015
+ * Group: Joel
+ * Verify that the AuthorManager works as intended.
+ */
+public class AuthorManagerTest {
+}
