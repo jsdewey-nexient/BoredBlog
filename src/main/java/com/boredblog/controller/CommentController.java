b01344c89393f106d80ae2bc3a69848048166660
Joel Dewey <jdewey@nexient.com>
2015-08-21 10:14:13 -0400

    Placed some annotations into CommentController.

--- src/main/java/com/boredblog/controller/CommentController.java
+++ src/main/java/com/boredblog/controller/CommentController.java
@@ -1,10 +1,15 @@
 package com.boredblog.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Joel Dewey
  * @date 8/21/2015
  * Group: Joel
  * Loads comments for a particular post.
  */
+@RestController
+@RequestMapping("comments")
 public class CommentController {
 }
