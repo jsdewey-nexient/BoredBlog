35323f142461711af1f6a15548bb7117671015d5
Joel Dewey <jdewey@nexient.com>
2015-08-21 14:59:32 -0400

    Created Guest entity test suite.

---
+++ src/test/java/com/boredblog/entity/GuestTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.entity;
+
+/**
+ * @author Joel Dewey
+ * @date 8/21/2015
+ * Group: Joel
+ * Verify that the Guest entity works as it should.
+ */
+public class GuestTest {
+}
