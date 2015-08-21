4051ff6bfd9dacf9aec053acab402f70684613f3
Joel Dewey <jdewey@nexient.com>
2015-08-21 12:46:37 -0400

    Created test class.

---
+++ src/test/java/com/boredblog/entity/AuthorTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.entity;
+
+/**
+ * @author Joel Dewey
+ * @date 8/21/2015
+ * Group: Joel
+ * Verify that the Author entity works as expected.
+ */
+public class AuthorTest {
+}
