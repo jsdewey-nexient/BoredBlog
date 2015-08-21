d4bc8d5acdea3428751158efe217f955113df8b6
Joel Dewey <jdewey@nexient.com>
2015-08-21 14:00:43 -0400

    Created BaseEntity test class.

---
+++ src/test/java/com/boredblog/entity/BaseEntityTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.entity;
+
+/**
+ * @author Joel Dewey
+ * @date 8/21/2015
+ * Group: Joel
+ * Verify that the BaseEntity works as it should.
+ */
+public class BaseEntityTest {
+}
