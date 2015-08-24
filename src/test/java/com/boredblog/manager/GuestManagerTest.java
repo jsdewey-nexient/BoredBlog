0736d5033c2a0abe0f3b781b42c046e5e467e26d
Joel Dewey <jdewey@nexient.com>
2015-08-24 12:49:35 -0400

    Created mock objects.

--- src/test/java/com/boredblog/manager/GuestManagerTest.java
+++ src/test/java/com/boredblog/manager/GuestManagerTest.java
@@ -1,5 +1,11 @@
 package com.boredblog.manager;
 
+import com.boredblog.repository.GuestRepository;
+import org.junit.Before;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 /**
  * @author Joel Dewey
  * @date 8/24/2015
@@ -7,4 +13,13 @@ package com.boredblog.manager;
  * Verify the correct behavior of the GuestManager.
  */
 public class GuestManagerTest {
+    @Mock
+    private GuestRepository guestRepository;
+    @InjectMocks
+    private GuestManager guestManager;
+
+    @Before
+    public void setup() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
 }
