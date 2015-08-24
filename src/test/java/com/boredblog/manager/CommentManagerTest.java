0182e1781dba107a14e1e549a9f8882488e3e757
Joel Dewey <jdewey@nexient.com>
2015-08-24 10:34:03 -0400

    Added mock code for CommentManager.

--- src/test/java/com/boredblog/manager/CommentManagerTest.java
+++ src/test/java/com/boredblog/manager/CommentManagerTest.java
@@ -1,5 +1,12 @@
 package com.boredblog.manager;
 
+import com.boredblog.repository.CommentRepository;
+import org.junit.Before;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
 /**
  * @author Joel Dewey
  * @date 8/24/2015
@@ -7,4 +14,13 @@ package com.boredblog.manager;
  * Verify the behavior of the CommentManager.
  */
 public class CommentManagerTest {
+    @Mock
+    private CommentRepository commentRepository;
+    @InjectMocks
+    private CommentManager commentManager;
+
+    @Before
+    public void setup() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
 }
