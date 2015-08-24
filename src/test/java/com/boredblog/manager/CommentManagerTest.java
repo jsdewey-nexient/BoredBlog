55c6f95b1c9b40898d78d4c02f65f7e9e168b9d1
Joel Dewey <jdewey@nexient.com>
2015-08-24 12:03:20 -0400

    Added test for updating a Comment.

--- src/test/java/com/boredblog/manager/CommentManagerTest.java
+++ src/test/java/com/boredblog/manager/CommentManagerTest.java
@@ -1,11 +1,17 @@
 package com.boredblog.manager;
 
+import com.boredblog.entity.Comment;
 import com.boredblog.repository.CommentRepository;
 import org.junit.Before;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.junit.Test;
+import org.mockito.*;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 /**
  * @author Joel Dewey
@@ -14,13 +20,62 @@ import org.mockito.MockitoAnnotations;
  * Verify the behavior of the CommentManager.
  */
 public class CommentManagerTest {
+    private final Integer POST_ID = 1;
+    private final Integer COMMENT_ID = 1;
+    private final Integer UPDATE_COMMENT_ID = 2;
     @Mock
     private CommentRepository commentRepository;
     @InjectMocks
     private CommentManager commentManager;
+    private Comment comment;
+    private List<Comment> commentList;
+    private Comment updatedComment;
 
     @Before
     public void setup() throws Exception {
         MockitoAnnotations.initMocks(this);
+        instantiateDependencies();
+        mockCommentRepository();
+        setCommentProperties();
+    }
+
+    @Test
+    public void testUpdatingComment() {
+        String originalValue = comment.getContent();
+        String updatedValue = null;
+        this.commentManager.update(
+                POST_ID,
+                COMMENT_ID,
+                updatedComment
+        );
+        updatedValue = comment.getContent();
+
+        assertNotEquals(
+                "testUpdatingComment received the same string back.",
+                originalValue,
+                updatedValue
+        );
+    }
+
+    private void instantiateDependencies() {
+        this.comment = new Comment();
+        this.commentList = new ArrayList<Comment>();
+        this.updatedComment = new Comment();
+    }
+
+    private void setCommentProperties() {
+        this.comment.setContent("Hello!");
+        this.updatedComment.setContent("Good bye!");
+    }
+
+    private void mockCommentRepository() {
+        Mockito.when(this.commentRepository.save(this.comment))
+                .thenReturn(this.comment);
+        Mockito.when(this.commentRepository.findOne(COMMENT_ID))
+                .thenReturn(this.comment);
+        Mockito.when(this.commentRepository.findAll())
+                .thenReturn(this.commentList);
+        Mockito.when(this.commentRepository.findOne(UPDATE_COMMENT_ID))
+                .thenReturn(this.comment);
     }
 }
