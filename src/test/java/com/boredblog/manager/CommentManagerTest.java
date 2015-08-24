11fbc1e8723e23e77ec5bf8d613e996f1cabbcaf
Joel Dewey <jdewey@nexient.com>
2015-08-24 10:09:05 -0400

    Created test suite for CommentManager.

---
+++ src/test/java/com/boredblog/manager/CommentManagerTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.manager;
+
+/**
+ * @author Joel Dewey
+ * @date 8/24/2015
+ * Group: Joel
+ * Verify the behavior of the CommentManager.
+ */
+public class CommentManagerTest {
+}
