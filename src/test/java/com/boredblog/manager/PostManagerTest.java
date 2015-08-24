c3bb2fe6559f790ab0008274fe33c0f47a4ceb15
Joel Dewey <jdewey@nexient.com>
2015-08-24 13:47:30 -0400

    Added mock code as well as created dependent objects.

--- src/test/java/com/boredblog/manager/PostManagerTest.java
+++ src/test/java/com/boredblog/manager/PostManagerTest.java
@@ -1,10 +1,15 @@
 package com.boredblog.manager;
 
+import com.boredblog.entity.Post;
 import com.boredblog.repository.PostRepository;
 import org.junit.Before;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Joel Dewey
@@ -13,13 +18,41 @@ import org.mockito.MockitoAnnotations;
  * Verify that the PostManager is working correctly.
  */
 public class PostManagerTest {
+    private final Integer POST_ID = 1;
     @Mock
     private PostRepository postRepository;
     @InjectMocks
     private PostManager postManager;
+    private Post post;
+    private Post updatedPost;
+    private List<Post> posts;
 
     @Before
     public void setup() throws Exception {
         MockitoAnnotations.initMocks(this);
+
+        this.createDependentObjects();
+        this.setDependentObjectProperties();
+        this.mockPostRepository();
+    }
+
+    private void createDependentObjects() {
+        this.post = new Post();
+        this.updatedPost = new Post();
+        this.posts = new ArrayList<Post>();
+    }
+
+    private void setDependentObjectProperties() {
+        this.post.setContent("Hello!");
+        this.updatedPost.setContent("Good bye!");
+    }
+
+    private void mockPostRepository() {
+        Mockito.when(this.postRepository.save(this.post))
+                .thenReturn(this.post);
+        Mockito.when(this.postRepository.findOne(POST_ID))
+                .thenReturn(this.post);
+        Mockito.when(this.postRepository.findAll())
+                .thenReturn(this.posts);
     }
 }
