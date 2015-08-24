f95df32c1f921f58201c7b2f7164dae1409b3af7
Joel Dewey <jdewey@nexient.com>
2015-08-24 12:48:17 -0400

    Created test suite for GuestManager.

---
+++ src/test/java/com/boredblog/manager/GuestManagerTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.manager;
+
+/**
+ * @author Joel Dewey
+ * @date 8/24/2015
+ * Group: Joel
+ * Verify the correct behavior of the GuestManager.
+ */
+public class GuestManagerTest {
+}
