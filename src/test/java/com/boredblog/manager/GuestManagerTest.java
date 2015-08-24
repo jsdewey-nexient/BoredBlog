971551682a5f928830a059c9de2e4087fefcfd93
Joel Dewey <jdewey@nexient.com>
2015-08-24 13:02:39 -0400

    Mocked some functions of the GuestRepository.

--- src/test/java/com/boredblog/manager/GuestManagerTest.java
+++ src/test/java/com/boredblog/manager/GuestManagerTest.java
@@ -1,10 +1,12 @@
 package com.boredblog.manager;
 
+import com.boredblog.entity.Guest;
 import com.boredblog.repository.GuestRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 /**
@@ -14,15 +16,20 @@ import org.mockito.MockitoAnnotations;
  * Verify the correct behavior of the GuestManager.
  */
 public class GuestManagerTest {
+    private final Integer GUEST_ID = 1;
     @Mock
     private GuestRepository guestRepository;
     @InjectMocks
     private GuestManager guestManager;
+    private Guest guest;
 
     @Before
     public void setup() throws Exception {
         MockitoAnnotations.initMocks(this);
+        this.mockGuestRepository();
     }
+
+
 
     @Test
     public void testCreateGuest() {
@@ -37,5 +44,12 @@ public class GuestManagerTest {
     @Test
     public void testUpdatingGuest() {
 
+    }
+
+    private void mockGuestRepository() {
+        Mockito.when(this.guestRepository.findOne(GUEST_ID))
+                .thenReturn(this.guest);
+        Mockito.when(this.guestRepository.save(this.guest))
+                .thenReturn(this.guest);
     }
 }
