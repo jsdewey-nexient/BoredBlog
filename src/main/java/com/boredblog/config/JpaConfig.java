e5c0a70bdd8f27ee9a2b63bd3c59a297158588c4
Joel Dewey <jdewey@nexient.com>
2015-08-25 10:46:28 -0400

    Created file for configuring the application to connect to the database.

---
+++ src/main/java/com/boredblog/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.boredblog.config;
+
+/**
+ * @author Joel Dewey
+ * @date 8/25/2015
+ * Group: Joel
+ * Configures the application to use Hibernate + JPA.
+ */
+public class JpaConfig {
+}
