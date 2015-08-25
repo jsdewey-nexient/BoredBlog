62da0b85b43489cf14bf672a4e2940e676ca9000
Joel Dewey <jdewey@nexient.com>
2015-08-25 11:13:07 -0400

    Added annotations.

--- src/main/java/com/boredblog/config/PropertyPlaceholderConfig.java
+++ src/main/java/com/boredblog/config/PropertyPlaceholderConfig.java
@@ -1,10 +1,16 @@
 package com.boredblog.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
 /**
  * @author Joel Dewey
  * @date 8/25/2015
  * Group: Joel
  * Imports properties from external files.
  */
+@Configuration
+@PropertySource({ "classpath:db.database.properties" })
 public class PropertyPlaceholderConfig {
+
 }
