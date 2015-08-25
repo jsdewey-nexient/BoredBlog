565abeed94a08e79dfdbc31029d5d1c5cfb37e7c
Joel Dewey <jdewey@nexient.com>
2015-08-25 10:49:16 -0400

    Added necessary annotations.

--- src/main/java/com/boredblog/config/JpaConfig.java
+++ src/main/java/com/boredblog/config/JpaConfig.java
@@ -1,10 +1,17 @@
 package com.boredblog.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 /**
  * @author Joel Dewey
  * @date 8/25/2015
  * Group: Joel
  * Configures the application to use Hibernate + JPA.
  */
+@Configuration
+@EnableJpaRepositories("com.boredblog.repository")
+@EnableTransactionManagement
 public class JpaConfig {
 }
