719c57ea6da5ba0114a3828ba752bbd49a2cef11
Joel Dewey <jdewey@nexient.com>
2015-08-25 11:19:57 -0400

    Created variables to hold database property values.

--- src/main/java/com/boredblog/config/JpaConfig.java
+++ src/main/java/com/boredblog/config/JpaConfig.java
@@ -1,5 +1,6 @@
 package com.boredblog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -14,4 +15,22 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories("com.boredblog.repository")
 @EnableTransactionManagement
 public class JpaConfig {
+    @Value("${database.driverClassName}")
+    private String driverClassName;
+    @Value("${database.url}")
+    private String url;
+    @Value("${database.username}")
+    private String username;
+    @Value("${database.password}")
+    private String password;
+    @Value("${database.showSql}")
+    private Boolean showSql;
+    @Value("${database.jdbc.maxPoolSize}")
+    private Integer maxPoolSize;
+    @Value("${database.jdbc.minPoolSize}")
+    private Integer minPoolSize;
+    @Value("${database.jdbc.maxStatements}")
+    private Integer maxStatements;
+    @Value("${database.jdbc.testConnection}")
+    private Boolean testConnection;
 }
