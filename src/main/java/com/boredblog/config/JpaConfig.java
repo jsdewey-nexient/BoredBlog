55ff586699fca6e0c72e3c2fb56743733779d004
Joel Dewey <jdewey@nexient.com>
2015-08-25 12:26:36 -0400

    Added import for PropertyPlaceHolder config file.

--- src/main/java/com/boredblog/config/JpaConfig.java
+++ src/main/java/com/boredblog/config/JpaConfig.java
@@ -4,6 +4,7 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -23,6 +24,7 @@ import java.beans.PropertyVetoException;
 @Configuration
 @EnableJpaRepositories("com.boredblog.repository")
 @EnableTransactionManagement
+@Import(PropertyPlaceholderConfig.class)
 public class JpaConfig {
     @Value("${database.driverClassName}")
     private String driverClassName;
