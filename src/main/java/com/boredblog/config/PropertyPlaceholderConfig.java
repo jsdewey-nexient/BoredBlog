2cf941fecb11a4640fd73641c0b63bef3e1842ac
Joel Dewey <jdewey@nexient.com>
2015-08-25 11:12:00 -0400

    Created configuration file for importing properties.

---
+++ src/main/java/com/boredblog/config/PropertyPlaceholderConfig.java
@@ -0,0 +1,10 @@
+package com.boredblog.config;
+
+/**
+ * @author Joel Dewey
+ * @date 8/25/2015
+ * Group: Joel
+ * Imports properties from external files.
+ */
+public class PropertyPlaceholderConfig {
+}
