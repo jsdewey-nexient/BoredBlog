45a490d0b97303ce04c409d807e15120d9460b2e
Joel Dewey <jdewey@nexient.com>
2015-08-26 15:09:38 -0400

    Forgot to prevent a bean being created of the BaseRepo.

--- src/main/java/com/boredblog/repository/BaseRepository.java
+++ src/main/java/com/boredblog/repository/BaseRepository.java
@@ -2,6 +2,7 @@ package com.boredblog.repository;
 
 import com.boredblog.entity.BaseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.io.Serializable;
 
@@ -11,6 +12,7 @@ import java.io.Serializable;
  * Group: Joel
  * A base repository for all other repositories to extend.
  */
+@NoRepositoryBean
 public interface BaseRepository<T extends BaseEntity, ID extends Serializable>
     extends JpaRepository<T, ID> {
 }
