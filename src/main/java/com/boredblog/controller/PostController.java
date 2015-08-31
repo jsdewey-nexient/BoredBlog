19d28f4a7fa26a6935aa480f900c3faadca30e47
Joel Dewey <jdewey@nexient.com>
2015-08-31 13:44:48 -0400

    Removed autowired annotation from field.

--- src/main/java/com/boredblog/controller/PostController.java
+++ src/main/java/com/boredblog/controller/PostController.java
@@ -16,7 +16,6 @@ import java.util.List;
 @RestController
 @RequestMapping("posts")
 public class PostController {
-    @Autowired
     private PostManager postManager;
 
     @RequestMapping(method = RequestMethod.GET)
