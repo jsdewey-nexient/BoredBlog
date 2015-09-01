8d9187e520457850ea36014a9505216c0ee3e849
Joel Dewey <jdewey@nexient.com>
2015-09-01 15:35:06 -0400

    Created JSON view for comments.

---
+++ src/main/java/com/boredblog/jsonview/CommentJsonView.java
@@ -0,0 +1,10 @@
+package com.boredblog.jsonview;
+
+/**
+ * @author Joel Dewey
+ * @date 9/1/2015
+ * Group: Joel
+ * Limits the information delivered regarding a comment.
+ */
+public interface CommentJsonView {
+}
