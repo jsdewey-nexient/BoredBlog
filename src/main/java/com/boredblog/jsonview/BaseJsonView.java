80c78659a482e25fda0446a33b839a9e94b7bb77
Joel Dewey <jdewey@nexient.com>
2015-09-01 16:18:00 -0400

    Created base json view file.

---
+++ src/main/java/com/boredblog/jsonview/BaseJsonView.java
@@ -0,0 +1,10 @@
+package com.boredblog.jsonview;
+
+/**
+ * @author Joel Dewey
+ * @date 9/1/2015
+ * Group: Joel
+ * A basic JsonView for shared properties.
+ */
+public interface BaseJsonView {
+}
