d4414c8ac43a25de1e04dc7efaeaaa9c82031a01
Joel Dewey <jdewey@nexient.com>
2015-09-01 14:33:55 -0400

    Added annotations.

--- src/test/java/com/boredblog/controller/comment/CommentControllerAllJsonTest.java
+++ src/test/java/com/boredblog/controller/comment/CommentControllerAllJsonTest.java
@@ -1,10 +1,25 @@
 package com.boredblog.controller.comment;
 
+import com.boredblog.config.JpaConfig;
+import com.boredblog.config.RootConfig;
+import com.boredblog.config.WebConfig;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
 /**
  * @author Joel Dewey
  * @date 9/1/2015
  * Group: Joel
  * Validate the JSON response when retrieving all comments for a post.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {
+        WebConfig.class,
+        RootConfig.class,
+        JpaConfig.class
+})
 public class CommentControllerAllJsonTest {
 }
