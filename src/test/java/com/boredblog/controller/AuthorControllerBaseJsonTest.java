711eee1ae4fffe452e7f3ef74bdbb000bf5f525a
Joel Dewey <jdewey@nexient.com>
2015-09-01 10:04:08 -0400

    Created a class to extract shared code out to.

---
+++ src/test/java/com/boredblog/controller/AuthorControllerBaseJsonTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.controller;
+
+/**
+ * @author Joel Dewey
+ * @date 9/1/2015
+ * Group: Joel
+ * A base class for the Author JSON tests to extend and share code.
+ */
+public class AuthorControllerBaseJsonTest {
+}
