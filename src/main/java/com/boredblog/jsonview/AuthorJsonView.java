179747489f485376344bc23218e5ca4c5a6596f3
Joel Dewey <jdewey@nexient.com>
2015-09-01 13:09:43 -0400

    Created JSON view for Authors.

---
+++ src/main/java/com/boredblog/jsonview/AuthorJsonView.java
@@ -0,0 +1,10 @@
+package com.boredblog.jsonview;
+
+/**
+ * @author Joel Dewey
+ * @date 9/1/2015
+ * Group: Joel
+ * An interface defining JSON views regarding Authors.
+ */
+public interface AuthorJsonView {
+}
