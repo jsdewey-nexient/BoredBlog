79a8daf55dbb76e6d7bfda8bea1ee996f7769018
Joel Dewey <jdewey@nexient.com>
2015-09-01 14:31:54 -0400

    Created test suite for validating the JSON response for retrieving all comments of a post.

---
+++ src/test/java/com/boredblog/controller/comment/CommentControllerAllJsonTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.controller.comment;
+
+/**
+ * @author Joel Dewey
+ * @date 9/1/2015
+ * Group: Joel
+ * Validate the JSON response when retrieving all comments for a post.
+ */
+public class CommentControllerAllJsonTest {
+}
