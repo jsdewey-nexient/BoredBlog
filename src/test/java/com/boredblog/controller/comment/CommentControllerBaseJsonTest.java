8811fbdad3e6f88b60196d91c90eb70aa1f6ddce
Joel Dewey <jdewey@nexient.com>
2015-09-02 10:07:28 -0400

    Created class to place common CommentController JSON tests.

---
+++ src/test/java/com/boredblog/controller/comment/CommentControllerBaseJsonTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.controller.comment;
+
+/**
+ * @author Joel Dewey
+ * @date 9/2/2015
+ * Group: Joel
+ * A class with shared setup methods.
+ */
+public class CommentControllerBaseJsonTest {
+}
