9ea3ba0381786e752277c2edce224e50d402f7a3
Joel Dewey <jdewey@nexient.com>
2015-09-02 09:04:48 -0400

    Created base abstract class for the JSON tests.

---
+++ src/test/java/com/boredblog/controller/BaseJsonTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.controller;
+
+/**
+ * @author Joel Dewey
+ * @date 9/2/2015
+ * Group: Joel
+ * A basic test class for the others to extend.
+ */
+public abstract class BaseJsonTest {
+}
