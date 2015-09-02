1253d856f789cb697d2a957e8a848a9beae94341
Joel Dewey <jdewey@nexient.com>
2015-09-02 09:55:47 -0400

    Created test suite for retrieving a single Comment.

---
+++ src/test/java/com/boredblog/controller/comment/CommentControllerSingleObjectJsonTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.controller.comment;
+
+/**
+ * @author Joel Dewey
+ * @date 9/2/2015
+ * Group: Joel
+ * Test the JSON response for a single Comment (/posts/{postId}/comments/{commentId})
+ */
+public class CommentControllerSingleObjectJsonTest {
+}
