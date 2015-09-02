570608e8939e8e9412072ab3c0fafcd0d54ca011
Joel Dewey <jdewey@nexient.com>
2015-09-02 16:09:01 -0400

    Created test suite.

---
+++ src/test/java/com/boredblog/controller/post/PostControllerSingleObjectJsonTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.controller.post;
+
+/**
+ * @author Joel Dewey
+ * @date 9/2/2015
+ * Group: Joel
+ * Test retrieving a single Post's JSON.
+ */
+public class PostControllerSingleObjectJsonTest extends PostControllerBaseJsonTest {
+}
