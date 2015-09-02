983bf673f3958bba5d01fdb5c564af3d62225d29
Joel Dewey <jdewey@nexient.com>
2015-09-02 12:23:28 -0400

    Added extension.

--- src/test/java/com/boredblog/controller/post/PostControllerAllJsonTest.java
+++ src/test/java/com/boredblog/controller/post/PostControllerAllJsonTest.java
@@ -1,10 +1,12 @@
 package com.boredblog.controller.post;
 
+import com.boredblog.controller.BaseJsonTest;
+
 /**
  * @author Joel Dewey
  * @date 9/2/2015
  * Group: Joel
  * Verify that the JSON for fetching a post feed is correct.
  */
-public class PostControllerAllJsonTest {
+public class PostControllerAllJsonTest extends BaseJsonTest {
 }
