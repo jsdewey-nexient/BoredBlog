b6402e28ad1e6576bf080f1f1b48b3f703273faf
Joel Dewey <jdewey@nexient.com>
2015-09-02 14:55:43 -0400

    Created JsonView for the User abstract class.

---
+++ src/main/java/com/boredblog/jsonview/UserJsonView.java
@@ -0,0 +1,10 @@
+package com.boredblog.jsonview;
+
+/**
+ * @author Joel Dewey
+ * @date 9/2/2015
+ * Group: Joel
+ * A JsonView for the User abstract class.
+ */
+public interface UserJsonView {
+}
