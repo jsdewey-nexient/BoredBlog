4be7a5c8433c546f79fb41489df2ae45586a5046
Joel Dewey <jdewey@nexient.com>
2015-09-02 16:12:04 -0400

    Created unique methods.

--- src/test/java/com/boredblog/controller/post/PostControllerSingleObjectJsonTest.java
+++ src/test/java/com/boredblog/controller/post/PostControllerSingleObjectJsonTest.java
@@ -1,5 +1,12 @@
 package com.boredblog.controller.post;
 
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
+
 /**
  * @author Joel Dewey
  * @date 9/2/2015
@@ -7,4 +14,16 @@ package com.boredblog.controller.post;
  * Test retrieving a single Post's JSON.
  */
 public class PostControllerSingleObjectJsonTest extends PostControllerBaseJsonTest {
+
+    private void mockPostManager() {
+        Mockito.when(this.postManager.retrieve(Mockito.anyInt()))
+                .thenReturn(this.post);
+    }
+
+    private void sendRequestToRetrieveAll() throws Exception {
+        this.response = this.mockMvc.perform(
+                MockMvcRequestBuilders.get("/posts/" + POST_ID)
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+    }
 }
