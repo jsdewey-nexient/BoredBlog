e8363746cbd821feaf719281af44f72d45d2a4e0
Joel Dewey <jdewey@nexient.com>
2015-09-02 16:00:13 -0400

    Created base test file.

---
+++ src/test/java/com/boredblog/controller/post/PostControllerBaseJsonTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.controller.post;
+
+/**
+ * @author Joel Dewey
+ * @date 9/2/2015
+ * Group: Joel
+ * A class containing shared methods.
+ */
+public class PostControllerBaseJsonTest {
+}
