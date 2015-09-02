28f65b8943e3f7e4c996223f9ee686f54aaf334e
Joel Dewey <jdewey@nexient.com>
2015-09-02 14:42:09 -0400

    Recreated the BaseJsonView.

---
+++ src/main/java/com/boredblog/jsonview/BaseJsonView.java
@@ -0,0 +1,10 @@
+package com.boredblog.jsonview;
+
+/**
+ * @author Joel Dewey
+ * @date 9/2/2015
+ * Group: Joel
+ * A base JsonView for the others to extend.
+ */
+public interface BaseJsonView {
+}
