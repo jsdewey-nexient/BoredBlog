2fbf76dd0919aaded76c96376f81e9569c616ddd
Joel Dewey <jdewey@nexient.com>
2015-09-04 14:50:45 -0400

    Added annotations to pull in values from properties file.

--- src/integration/java/com/boredblog/entity/PostValidatorTest.java
+++ src/integration/java/com/boredblog/entity/PostValidatorTest.java
@@ -1,10 +1,21 @@
 package com.boredblog.entity;
 
+import com.boredblog.config.JpaConfig;
+import com.boredblog.config.PropertyPlaceholderConfig;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * @author Joel Dewey
  * @date 9/4/2015
  * Group: Joel
  * Verify that the Hibernate Validator is working for the Post entity.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {
+        PropertyPlaceholderConfig.class,
+        JpaConfig.class
+})
 public class PostValidatorTest {
 }
