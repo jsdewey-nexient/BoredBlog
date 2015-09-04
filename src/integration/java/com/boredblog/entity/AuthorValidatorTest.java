73b088f985090b4a2e04e27d5be7f4bd3a96bfbe
Joel Dewey <jdewey@nexient.com>
2015-09-04 12:52:52 -0400

    Created test suite for the Author validator.

---
+++ src/integration/java/com/boredblog/entity/AuthorValidatorTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.entity;
+
+/**
+ * @author Joel Dewey
+ * @date 9/4/2015
+ * Group: Joel
+ * Validate that the Hibernate Validator is acting accordingly.
+ */
+public class AuthorValidatorTest {
+}
