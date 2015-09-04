f0228303afc9712792dd11bcfec9a7d35681dc4d
Joel Dewey <jdewey@nexient.com>
2015-09-04 14:51:31 -0400

    Added code for creating the Validator.

--- src/integration/java/com/boredblog/entity/PostValidatorTest.java
+++ src/integration/java/com/boredblog/entity/PostValidatorTest.java
@@ -2,9 +2,15 @@ package com.boredblog.entity;
 
 import com.boredblog.config.JpaConfig;
 import com.boredblog.config.PropertyPlaceholderConfig;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 /**
  * @author Joel Dewey
@@ -18,4 +24,16 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
         JpaConfig.class
 })
 public class PostValidatorTest {
+    private static Validator validator;
+
+    @Value("${post.title.NotBlank}")
+    private String postTitleNotBlankMessage;
+    @Value("${post.content.NotBlank}")
+    private String postContentNotBlankMessage;
+
+    @BeforeClass
+    public static void beforeSetup() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
 }
