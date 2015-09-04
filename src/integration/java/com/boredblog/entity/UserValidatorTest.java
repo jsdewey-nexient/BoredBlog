158d2f2a827b8b8e357d21a66dba19359dad2a4c
Joel Dewey <jdewey@nexient.com>
2015-09-04 14:56:27 -0400

    Created validator test for User.

---
+++ src/integration/java/com/boredblog/entity/UserValidatorTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.entity;
+
+/**
+ * @author Joel Dewey
+ * @date 9/4/2015
+ * Group: Joel
+ * Validates that the Validator in the abstract class User is working.
+ */
+public class UserValidatorTest {
+}
