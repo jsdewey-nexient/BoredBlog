c197ab40764f2755bf4765992a2d432021ee561c
Joel Dewey <jdewey@nexient.com>
2015-09-04 14:55:34 -0400

    Added tests to PostValidator.

--- src/integration/java/com/boredblog/entity/PostValidatorTest.java
+++ src/integration/java/com/boredblog/entity/PostValidatorTest.java
@@ -3,14 +3,19 @@ package com.boredblog.entity;
 import com.boredblog.config.JpaConfig;
 import com.boredblog.config.PropertyPlaceholderConfig;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Joel Dewey
@@ -35,5 +40,69 @@ public class PostValidatorTest {
     public static void beforeSetup() {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
+    }
+
+    @Test
+    public void titleIsEmpty() {
+        Post post = new Post();
+        post.setTitle("");
+        post.setContent("This is content.");
+
+        Set<ConstraintViolation<Post>> violations
+                = validator.validate(post);
+
+        assertEquals(1, violations.size());
+        assertEquals(
+                this.postTitleNotBlankMessage,
+                violations.iterator().next().getMessage()
+        );
+    }
+
+    @Test
+    public void titleIsNull() {
+        Post post = new Post();
+        post.setTitle(null);
+        post.setContent("This is content.");
+
+        Set<ConstraintViolation<Post>> violations
+                = validator.validate(post);
+
+        assertEquals(1, violations.size());
+        assertEquals(
+                this.postTitleNotBlankMessage,
+                violations.iterator().next().getMessage()
+        );
+    }
+
+    @Test
+    public void contentIsEmpty() {
+        Post post = new Post();
+        post.setTitle("This is a title.");
+        post.setContent("");
+
+        Set<ConstraintViolation<Post>> violations
+                = validator.validate(post);
+
+        assertEquals(1, violations.size());
+        assertEquals(
+                this.postContentNotBlankMessage,
+                violations.iterator().next().getMessage()
+        );
+    }
+
+    @Test
+    public void contentIsNull() {
+        Post post = new Post();
+        post.setTitle("This is a title.");
+        post.setContent(null);
+
+        Set<ConstraintViolation<Post>> violations
+                = validator.validate(post);
+
+        assertEquals(1, violations.size());
+        assertEquals(
+                this.postContentNotBlankMessage,
+                violations.iterator().next().getMessage()
+        );
     }
 }
