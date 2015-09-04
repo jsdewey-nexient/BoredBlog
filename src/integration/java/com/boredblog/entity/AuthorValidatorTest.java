678876e01d67eb5ca761a94128c4ec84954b8f90
Joel Dewey <jdewey@nexient.com>
2015-09-04 12:55:13 -0400

    Created validator factory.

--- src/integration/java/com/boredblog/entity/AuthorValidatorTest.java
+++ src/integration/java/com/boredblog/entity/AuthorValidatorTest.java
@@ -1,5 +1,11 @@
 package com.boredblog.entity;
 
+import org.junit.BeforeClass;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
 /**
  * @author Joel Dewey
  * @date 9/4/2015
@@ -7,4 +13,11 @@ package com.boredblog.entity;
  * Validate that the Hibernate Validator is acting accordingly.
  */
 public class AuthorValidatorTest {
+    private static Validator validator;
+
+    @BeforeClass
+    public static void beforeSetup() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
 }
