3146ad6295c547c2b78be54273a294ba9006c7e9
Joel Dewey <jdewey@nexient.com>
2015-09-04 13:50:58 -0400

    Created test suite for the Validator in the Comment entity.

---
+++ src/integration/java/com/boredblog/entity/CommentValidatorTest.java
@@ -0,0 +1,10 @@
+package com.boredblog.entity;
+
+/**
+ * @author Joel Dewey
+ * @date 9/4/2015
+ * Group: Joel
+ * Verify that the Hibernate Validator is working for the Comment entity.
+ */
+public class CommentValidatorTest {
+}
